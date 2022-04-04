d6ed3044c9dce7fa8afda35fee12d543bbb0cde9
Jean Fernandine <jfernandine@gmail.com>
2022-04-04 16:00:59 -0300

    novo-app

---
+++ src/model/task.java
@@ -0,0 +1,111 @@
+package model;
+
+import java.util.Date;
+
+public class task {
+
+	private int id;
+	private int id_project;
+	private String name;
+	private String description;
+	private boolean completed;
+	private String notes;
+	private Date deadline;
+	private Date createdAt;
+	private Date updateAt;
+
+	public task(int id, int id_project, String name, String description, boolean completed, String notes, Date deadline,
+			Date createdAt, Date updateAt) {
+		super();
+		this.id = id;
+		this.id_project = id_project;
+		this.name = name;
+		this.description = description;
+		this.completed = completed;
+		this.notes = notes;
+		this.deadline = deadline;
+		this.createdAt = createdAt;
+		this.updateAt = updateAt;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId_project() {
+		return id_project;
+	}
+
+	public void setId_project(int id_project) {
+		this.id_project = id_project;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public Date getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(Date updateAt) {
+		this.updateAt = updateAt;
+	}
+
+	@Override
+	public String toString() {
+		return "task [id=" + id + ", id_project=" + id_project + ", name=" + name + ", description=" + description
+				+ ", completed=" + completed + ", notes=" + notes + ", deadline=" + deadline + ", createdAt="
+				+ createdAt + ", updateAt=" + updateAt + "]";
+	}
+
+	
+}
---
+++ src/novoApp/Main.java
@@ -0,0 +1,53 @@
+package novoApp;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class Main {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+	}
+
+}
